60b93c7436cbcad0066e3992ed31872dc108126c
Eric Martinez <eric@ericmartinez.dev>
2024-01-11 04:27:34 -0500

    initial icodemas #1

---
+++ icodemas-roadmap/icodemas-01/index.js
@@ -0,0 +1,25 @@
+// Set the date we're counting down to
+let christmas = new Date("Dec 25, 2024 00:00:00").getTime();
+
+// Update the count down every 1 second
+let x = setInterval(function () {
+    // Get today's date and time
+    let now = new Date().getTime();
+
+     // Find the distance between now and the count down date
+    let distance = christmas - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element by grabbing class names
+    document.getElementsByClassName("day-number")[0].innerHTML = days
+    document.getElementsByClassName("day-hours")[0].innerHTML = hours
+    document.getElementsByClassName("day-mins")[0].innerHTML = minutes
+    document.getElementsByClassName("day-seconds")[0].innerHTML = seconds
+
+
+}, 1000)
